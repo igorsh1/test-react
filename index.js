2b4a388cf6a9f297b2a7932493a8c443d890efcf
igorsh1 <igor.sh.1980.sh@gmail.com>
2021-07-25 17:13:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/*class App extends React.Component {
+        render() {
+          return <h1>Hello world!</h1>
+        }
+      }
+*/
+ReactDOM.render(React.createElement('h1', null, 'Hello world!'), document.getElementById('root'))
